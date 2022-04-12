3b6bd02e691d1ccdd06fa1d005a1695ff9e71dc9
Sirojiddin Khujabekov <xujabekovsirojiddin@gmai.com>
2022-04-12 16:04:33 +0500

    commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+type Num int
+
+const (
+	SidesTriangle Num = 3
+	SidesSquare   Num = 4
+	SidesCircle   Num = 0
+	Pi                = 3.14
+)
+
+func CalcSquare(sideLen float64, sidesNum Num) float64 {
+	if sidesNum == SidesTriangle {
+		squareTriangle := (math.Sqrt(3) * (math.Pow(sideLen, 2))) / 4
+		return squareTriangle
+	}
+	if sidesNum == SidesSquare {
+		squareSquare := math.Pow(sideLen, 2)
+		return squareSquare
+	}
+	if sidesNum == SidesCircle {
+		squareCircle := Pi * math.Pow(sideLen, 2)
+		return squareCircle
+	}
+	return 0
+}
